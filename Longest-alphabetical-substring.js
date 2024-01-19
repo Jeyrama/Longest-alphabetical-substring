9fcd97b9f98244ee637cbe548273ca636c0ec532
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-19 15:58:02 -0800

    Create Longest-alphabetical-substring.js

---
+++ Longest-alphabetical-substring.js
@@ -0,0 +1,7 @@
+/*
+Find the longest substring in alphabetical order.
+The input will only consist of lowercase characters and will be at least one letter long.
+
+Example: 
+  the longest alphabetical substring in "asdfaaaabbbbcttavvfffffdf" is "aaaabbbbctt".
+*/
