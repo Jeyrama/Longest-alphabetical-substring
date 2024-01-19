e6fa9fe659215bad72a11d1773429d4ed9ce1700
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-19 15:59:13 -0800

    Update Longest-alphabetical-substring.js

--- Longest-alphabetical-substring.js
+++ Longest-alphabetical-substring.js
@@ -8,3 +8,21 @@ Example:
 
 
 // Solution
+
+function longest(str) {
+  let max = 0;
+  let cur = 0;
+  let pos = 0;
+  for (let i = 1; i < str.length; ++i) {
+    if (str[i-1] <= str[i]) {
+      ++cur;
+      if (cur > max) {
+        max = cur;
+        pos = i - max;
+      }
+    } else {
+      cur = 0;
+    }
+  }
+  return str.slice(pos, max+1+pos);
+}
